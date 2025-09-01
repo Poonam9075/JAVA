10a26e067eaeb7d03386421dcd7fba5f679405a6
Poonam Khandagale <130628612+Poonam9075@users.noreply.github.com>
2025-09-01 21:59:50 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,20 @@
+/* Accept 5 doubles from user (scanner)
+Print it's average.*/
+
+import java.util.Scanner;
+class Average{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter 5 double values: ");
+double arr[] = new double[5]; 
+double sum=0, avg=0;
+for(int i=0; i<5; i++){
+arr[i]=sc.nextDouble();
+}
+for(int i=0; i<5; i++){
+sum=sum+arr[i];
+}
+avg = sum/5;
+System.out.println("Average of 5 double values = "+avg);
+}
+}
